7c5dab1a812142be5fa9baa8f573b3b9eba1bf50
LeeSuGang <71600110+SuGangLee@users.noreply.github.com>
2021-08-17 05:36:41 +0900

    Add files via upload

---
+++ api-server-node/config/database.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2/promise');
+const {logger} = require('./winston');
+
+// TODO: 본인의 DB 계정 입력
+const pool = mysql.createPool({
+    host: 'lisugangdb.cf6svnf8io2k.ap-northeast-2.rds.amazonaws.com',
+    user: 'lisugang',
+    port: '3306',
+    password: 'su3081802!',
+    database: 'yanolja'
+});
+
+module.exports = {
+    pool: pool
+};
